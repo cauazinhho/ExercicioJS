4e85077dd4ddddb56654418f12bad16df3760a7d
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-02 17:01:36 -0300

    feat: Exemplo

---
+++ script.js
@@ -0,0 +1,22 @@
+// Escreva um programa que solicite o nome do usuario e lhe de boas vindas,
+// exibindo uma mensagem pelo console.
+
+//   prompt=mostrar mensagem
+// let nome= variavel que vai guardar no nome
+let nome = prompt("Ola,digite seu nome: ")
+
+// "entao me mostre no console a variavel nome".
+// concatenacao (+) e para grudar dois nomes
+console.log("Seja Bem " + nome + "!!!");
+
+let pontuacaoTotal = pontos + bonus
+let pontos = 100
+
+// Transforma o que esta entre () em número inteiro
+let bonus = parseInt( prompt("Quantos pontos você deseja somar?"))
+
+let notaAluno = parseFloat(prompt("Digite a nota do aluno: "))
+
+
+
+console.log(pontos + bonus)

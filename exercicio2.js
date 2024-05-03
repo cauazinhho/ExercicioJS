e22337378b55f7926859a41e2c2b83c7e0c6b329
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-03 15:24:33 -0300

    feat: atividade 2 feita

---
+++ exercicio2.js
@@ -0,0 +1,38 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
+
+let dia = prompt("Digite o dia que você deseja")
+
+
+
+if (dia == "1"){
+    console.log("segunda-feira")
+}
+
+if (dia== "2"){
+console.log("Terça-feira")
+}
+
+if (dia== "3"){
+console.log("Quarta-feira")
+}
+
+if (dia== "4"){
+console.log("Quinta-feira")
+}
+
+if (dia== "5"){
+console.log("Sexta-feira")
+}
+
+if (dia== "6"){
+console.log("Sabado")
+}
+
+if (dia== "7"){
+console.log("Domingo")
+}
+
+
+
+
